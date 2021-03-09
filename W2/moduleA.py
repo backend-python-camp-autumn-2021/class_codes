7ca20d051cda253760414ee19920dcfe3bd8c4fe
Teghfo <teghfo@gmail.com>
2021-03-09 15:38:01 +0330

    start django

--- W2/moduleA.py
+++ W2/moduleA.py
@@ -1,2 +1,31 @@
 a = 10
 print("hello")
+
+
+class PropTest:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.fullname = firstname + " " + lastname
+
+    @property
+    def fullname(self):
+        print("in property method")
+        return self._fullname
+
+    @fullname.setter
+    def fullname(self, data):
+        print("in setter")
+        temp = data.split(" ")
+        if len(temp) > 1:
+            self.firstname = temp[0]
+            self.lastname = temp[1]
+        self._fullname = data
+
+
+obj = PropTest("nane", "ghsemi")
+
+
+# obj.fullname = "akbar ghasemi"
+
+print(obj.fullname)
