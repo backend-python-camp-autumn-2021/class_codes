8f20368208d914965b2a37f9ebc766fc93b3fefc
Teghfo <teghfo@gmail.com>
2021-04-29 13:41:46 +0430

    custom admin to suplier

--- w4-w5/product_present/admin.py
+++ w4-w5/product_present/admin.py
@@ -3,7 +3,40 @@ from django.contrib import admin
 from . import  models
 
 
-admin.site.register(models.HandProduct)
+class HandProductModelAdmin(admin.ModelAdmin):
+    exclude = ['slug']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(supplier__user=request.user)
+
+    def has_view_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        if obj is not None and obj.supplier.user != request.user:
+            return False
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        if request.user.is_superuser:
+            return True
+        if obj is not None and obj.supplier.user != request.user:
+            return False
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        if obj is not None and obj.supplier.user != request.user:
+            return False
+        return True
+
+
+admin.site.register(models.HandProduct, HandProductModelAdmin)
 admin.site.register(models.HandProductCity)
 admin.site.register(models.HandProductCat)
 admin.site.register(models.HandProductComment)
