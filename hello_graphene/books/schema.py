4e2d76e892182186c5f69aefd34aa4b88dcbfdd1
Teghfo <teghfo@gmail.com>
2021-05-20 12:31:25 +0430

    complete ex1

--- hello_graphene/books/schema.py
+++ hello_graphene/books/schema.py
@@ -1,4 +1,5 @@
 import graphene
+
 from graphene_django.types import DjangoObjectType, ObjectType
 
 from .models import Author, Book
@@ -20,3 +21,24 @@ class Query(ObjectType):
     book = graphene.Field(BookType, id=graphene.Int())
     all_authors = graphene.List(AuthorType)
     all_books = graphene.List(BookType)
+
+    def resolve_author(self, info, id):
+        try:
+            return Author.objects.get(id=id)
+        except Author.DoseNotExists:
+            return None
+
+    def resolve_book(self, info, id):
+        try:
+            return Book.objects.get(id=id)
+        except Book.DoesNotExist:
+            return None
+
+    def resolve_all_authors(self, info):
+        return Author.objects.all()
+
+    def resolve_all_books(self, info):
+        return Book.objects.all()
+
+
+schema = graphene.Schema(query=Query)
