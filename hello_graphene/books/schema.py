c5bdc38b62f0abc37ad827ba61866e1d9dea86fa
Teghfo <teghfo@gmail.com>
2021-05-21 12:09:09 +0430

    complete mutation

--- hello_graphene/books/schema.py
+++ hello_graphene/books/schema.py
@@ -1,14 +1,22 @@
 import graphene
 
+from django.shortcuts import get_object_or_404
+
 from graphene_django.types import DjangoObjectType, ObjectType
 
 from .models import Author, Book
 
 
+# class AuthorType(ObjectType):
+#     id = graphene.ID()
+#     name = graphene.String()
+#     birth_year = graphene.Int()
+
 class AuthorType(DjangoObjectType):
+
     class Meta:
         model = Author
-        # fields = ("id", "name")
+        # exclude = ("id",)
 
 
 class BookType(DjangoObjectType):
@@ -19,10 +27,11 @@ class BookType(DjangoObjectType):
 class Query(ObjectType):
     author = graphene.Field(AuthorType, id=graphene.Int())
     book = graphene.Field(BookType, id=graphene.Int())
-    all_authors = graphene.List(AuthorType)
+    all_authors = graphene.List(AuthorType, name="_all_authors")
     all_books = graphene.List(BookType)
+    ok = graphene.Boolean()
 
-    def resolve_author(self, info, id):
+    def resolve_author(root, info, id):
         try:
             return Author.objects.get(id=id)
         except Author.DoseNotExists:
@@ -40,6 +49,10 @@ class Query(ObjectType):
     def resolve_all_books(self, info):
         return Book.objects.all()
 
+    def resolve_ok(self, info):
+        print(info.context.user)
+        return True
+
 
 class AuthorInput(graphene.InputObjectType):
     id = graphene.ID()
@@ -53,13 +66,18 @@ class CreateAuthor(graphene.Mutation):
 
     created = graphene.Boolean()
     author = graphene.Field(AuthorType)
+    error_msg = graphene.String()
 
     @staticmethod
-    def mutate(root, info, input=None):
-        created = True
-        author_obj = Author(name=input.name, birth_year=input.birth_year)
-        author_obj.save()
-        return CreateAuthor(created=created, author=author_obj)
+    def mutate(self, info, input=None):
+        try:
+            author_obj = Author.objects.create(
+                name=input.name, birth_year=input.birth_year)
+            created = True
+            return CreateAuthor(created=created, author=author_obj, error_msg=None)
+        except Exception as e:
+            created = False
+            return CreateAuthor(created=created, author=None, error_msg=e)
 
 
 class UpdateAuthor(graphene.Mutation):
@@ -68,12 +86,12 @@ class UpdateAuthor(graphene.Mutation):
         input = AuthorInput(required=True)
 
     created = graphene.Boolean()
-    actor = graphene.Field(AuthorType)
+    author = graphene.Field(AuthorType)
 
     @staticmethod
-    def mutate(root, info, id, input=None):
+    def mutate(self, info, id, input=None):
         created = False
-        author_obj = Author.objects.get(pk=id)
+        author_obj = get_object_or_404(Author, pk=id)
         if author_obj:
             created = True
             author_obj.name = input.name
--- hello_graphene/books/urls.py
+++ hello_graphene/books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from .schema import schema
 
 urlpatterns = [
-    path('', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
