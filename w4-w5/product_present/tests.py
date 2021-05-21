601f7a81dcbe57aeb8ade1a637bba4e554d97f8e
Teghfo <teghfo@gmail.com>
2021-05-21 14:35:04 +0430

    handproduct app: write test functionality

--- w4-w5/product_present/tests.py
+++ w4-w5/product_present/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import HandProduct
+
+# Model Test #
+
+
+class HandProductModelTest(TestCase):
+    def setUp(self) -> None:
+        self.product = HandProduct(name="ru farshi")
+
+    def test_hand_product_name_label(self):
+        name = self.product._meta.get_field('name').verbose_name
+        self.assertEqual(name, 'name')
+
+    def test_hand_product_name_max_length(self):
+        name_max_length = self.product._meta.get_field('name').max_length
+        self.assertEqual(name_max_length, 255)
+
+
+# view test #
+
+class HandProductViewTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_url_hand_product_detail(self):
+        url = reverse('hand-product-detail', kwargs={"slug":self.product.slug})
+        self.assertEqual(url, f'/product_present/handproduct/{self.product.slug}/')
+
+    def test_hand_product_detail(self):
+        response = self.client.get(reverse('hand-product-detail', kwargs={"slug":"hello"}))
+        print(response.status_code)
